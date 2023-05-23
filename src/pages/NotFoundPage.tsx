b0e09e5648a6d352dc4fe6ea9c26e49c6856beca
SomashMargarita <maargaritasivceva@gmail.come>
2023-05-23 14:35:07 +0300

    add page not found

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-
-function NotFoundPage() {
-    return <div>NotFoundPage</div>;
-}
+import { NavLink } from "react-router-dom";
+import { FC } from "react";
+import "./style/notFoundPageStyle.css";
+const NotFoundPage: React.FC = () => {
+    return (
+        <div className="not-foud_page">
+            This page doesn&apos;t exist... Go&nbsp;
+            <NavLink to="/">
+                <span>Main page</span>
+            </NavLink>
+        </div>
+    );
+};
 
 export default NotFoundPage;
